c7788f083ce9d3a1f16d00a4f37d606aae045e0f
kareem-saber20 <169073435+kareem-saber20@users.noreply.github.com>
2026-01-12 23:09:00 +0200

    Add files via upload

--- Tasks/Task1.java
+++ Tasks/Task1.java
@@ -10,20 +10,121 @@ public class Task1 {
         
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Numbers without spaces: ");
-        String x = input.next();
-        
-        for (int i= 0 ; i <= x.length()-1 ; i++) {
+        System.out.print("Enter a Number: ");
 
-            // 48 >> uni code of number 0
+        int a = input.nextInt();
 
-            sum =  sum +  x.charAt(i) - 48;
+
+        // Example 1
+
+        if(a < 10){
+
+            sum = a;
             
-        } 
-        
-        System.out.println( "The sum of numbers: "  + sum); 
+        }
+
+        // Example 12
+
+        else if(a < 100 ){
+
+            int lastDigit = a / 10 ; // get 1
+            
+            int digit1 = a % 10 ;    // get 2
+            
+            sum = lastDigit + digit1;
+
+        }
+
+        // Example 123
+
+        else if(a < 1000){
+
+            int lastDigit = a / 100 ; // get 1
+
+            int digit1 = a % 10 ;     // get 3
+
+            int digit2 = (a / 10) % 10; // get 2
+
+            sum = lastDigit + digit1 + digit2 ; 
+
+        }
+
+        // Example 1234
+
+        else if (a < 10000){
+
+            int lastDigit = a / 1000 ; //get 1
+
+            int digit1 = a % 10 ;      //get 4
+
+            int digit2 = (a / 10) % 10; //get 3
+            
+            int digit3 = (a / 100) % 10; // get 2
+
+            sum = lastDigit + digit1 + digit2 + digit3;
+        }  
+
+        // Example 12345
+
+        else if (a < 100000){
+
+            int lastDigit = a / 10000 ; //get 1
+
+            int digit1 = a % 10 ;      //get 5
+
+            int digit2 = (a / 10) % 10; //get 4
+            
+            int digit3 = (a / 100) % 10; // get 3
+
+            int digit4 = (a / 1000) % 10; // get 2
+
+            sum = lastDigit + digit1 + digit2 + digit3 + digit4;
+
+        }
+
+        // Example 123456
+
+        else if (a < 1000000){
+
+            int lastDigit = a / 100000 ; // get 1
+
+            int digit1 = a % 10 ;        // get 6
+
+            int digit2 = (a / 10) % 10; //get 5
+            
+            int digit3 = (a / 100) % 10; // get 4
+
+            int digit4 = (a / 1000) % 10; // get 3
+
+            int digit5 = (a / 10000) % 10; // get 2
+
+            sum = lastDigit + digit1 + digit2 + digit3 + digit4 + digit5;
 
 
+        }  
+
+        // Example 1234567
+
+        else if (a < 10000000){
+
+            int lastDigit = a / 1000000 ; // get 1
+
+            int digit1 = a % 10 ;         // get 7
+
+            int digit2 = (a / 10) % 10; //get 6
+            
+            int digit3 = (a / 100) % 10; // get 5
+
+            int digit4 = (a / 1000) % 10; // get 4
+
+            int digit5 = (a / 10000) % 10; // get 3
+
+            int digit6 = (a / 100000) % 10; // get 2
+
+            sum = lastDigit + digit1 + digit2 + digit3 + digit4 + digit5 + digit6;
+        }
+
+        System.out.println("The sum: " + sum);
     }
     
 }
