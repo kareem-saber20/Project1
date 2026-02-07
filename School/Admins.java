cd35f1b60cf94b045aa7dfead6ff0ed98272a665
kareem-saber20 <169073435+kareem-saber20@users.noreply.github.com>
2026-02-07 17:30:25 +0200

    Add files via upload

--- School/Admins.java
+++ School/Admins.java
@@ -1,14 +1,13 @@
 package School;
 
-import java.util.ArrayList;
-
-public class Admins  extends  PersonalInformation{
+public class Admins extends PersonalInformation {
 
     private String password;
 
+
     //Constructors
 
-    Admins(String name, String gender, int id, String phone, String email) {
+    Admins(String name, String gender, String id, String phone, String email) {
 
         super(name, gender, id, phone, email);
 
@@ -16,50 +15,56 @@ public class Admins  extends  PersonalInformation{
 
     //....................................................
 
-    public void setNewStudents(){
+    public void setNewStudents(String name, String gender, String id, String phone, String email) {
 
+        Students student = new Students(name, gender, id, phone, email);
     }
 
-    public void getStudentsData(){
+    public String[] getStudentsData(Students student) {
 
+        String[] studentData = {student.name, student.gender, student.id, student.getPhone(), student.getEmail()};
+
+        return studentData;
     }
 
-    public void deleteStudents(){
-
-    }
-
-    //....................................................
-
-    public void setNewInstructors(){
-
-    }
-
-    public void getInstructorsData(){
-
-    }
-
-    public void deleteInstructors){
+    public void deleteStudents() {
 
     }
 
     //....................................................
 
+    public void setNewInstructors(String name, String gender, String id, String phone, String email) {
 
-    public void setNewCourses(){
-
+        Instructors instructor = new Instructors(name, gender, id, phone, email);
     }
 
-    public void getCoursesData(){
+    public String[] getInstructorsData(Instructors instructor) {
 
+        String[] instructorData = {instructor.name, instructor.gender, instructor.id, instructor.getPhone(), instructor.getEmail()};
+
+        return instructorData ;
     }
 
-    public void deleteCourses(){
+    public void deleteInstructors() {
 
     }
 
     //....................................................
 
 
+    public void setNewCourses(Courses course) {
+        course.setNewCourse(course);
+    }
+
+    public void getCoursesData(Courses course) {
+            course.getListOfCoursesName();
+    }
+
+    public void deleteCourses() {
+
+    }
+
+    //....................................................
 
 
 }
